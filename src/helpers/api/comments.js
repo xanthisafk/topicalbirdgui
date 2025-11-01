6a797ddbe744240284fe8117737a2aff54f36b53
Abhinav <abhinav.ranger@gmail.com>
2025-11-02 01:24:16 +0530

    Add: Comments to API helper

---
+++ src/helpers/api/comments.js
@@ -0,0 +1,52 @@
+import { API_ENDPOINTS } from "../../../config";
+import makeAxiosRequest from "../makeAxiosRequest";
+
+const api = API_ENDPOINTS.comments;
+
+export const deleteComment = async (id) => {
+    if (!id) return { status: 400, data: { message: "Comment ID is required."}};
+    const endpoint = api.delete;
+    const options = {
+        method: endpoint.method,
+        url: endpoint.url(id)
+    };
+    return await makeAxiosRequest(options);
+}
+
+export const getAllCommentOfPosts = async (id) => {
+    if (!id) return { status: 400, data: { message: "Post ID is required."}};
+    const endpoint = api.get;
+    const options = {
+        method: endpoint.method,
+        url: endpoint.url(id)
+    };
+    return await makeAxiosRequest(options);
+}
+
+export const updateComment = async (id, content) => {
+    if (!id) return { status: 400, data: { message: "Comment ID is required."}};
+    if (content.length < 1 || content.length > 10_000) return { status: 400, data: { message: "Comment size must be between 1 and 10,000 characters."}}
+    const form = new FormData();
+    form.append("Content", content);
+    const endpoint = api.update;
+    const options = {
+        method: endpoint.method,
+        url: endpoint.url(id),
+        data: form
+    }
+    return await makeAxiosRequest(options);
+}
+
+export const createNewComment = async (id, content) => {
+    if (!id) return { status: 400, data: { message: "Post ID is required."}};
+    if (content.length < 1 || content.length > 10_000) return { status: 400, data: { message: "Comment size must be between 1 and 10,000 characters."}}
+    const form = new FormData();
+    form.append("Content", content);
+    const endpoint = api.create;
+    const options = {
+        method: endpoint.method,
+        url: endpoint.url(id),
+        data: form
+    }
+    return await makeAxiosRequest(options);
+}
--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -2,3 +2,4 @@ export * from "./auth";
 export * from "./posts";
 export * from "./users"
 export * from "./nests";
+export * from "./comments"
