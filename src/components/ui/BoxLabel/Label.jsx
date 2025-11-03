8aaded1804c1f4d402dee5629ef1d77fd3db84bc
Abhinav <abhinav.ranger@gmail.com>
2025-11-03 06:42:10 +0530

    Update: Labels can have helper text

--- src/components/ui/BoxLabel/Label.jsx
+++ src/components/ui/BoxLabel/Label.jsx
@@ -1,10 +1,15 @@
+import InfoTooltip from '../InfoTooltip';
 import './label.css';
 
-const InputLabel = ({ htmlFor, children, className = '' }) => {
+
+const InputLabel = ({ htmlFor, children, className = '', helperText="" }) => {
   return (
+    <div className='boxlabel-container'>
     <label htmlFor={htmlFor} className={`input-label ${className}`}>
       {children}
     </label>
+    {helperText && <InfoTooltip text={helperText} />}
+    </div>
   );
 };
 
