c3c83c79207c099c7313f6357bb35f83f71f0904
Abhinav <abhinav.ranger@gmail.com>
2025-11-05 03:58:58 +0530

    Add: Button Component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,42 @@
+import "@/styles/components/ui/button.css";
+
+const Button = ({
+    variant = 'primary',
+    isIcon = false,
+    className = '',
+    children,
+    ...props
+}) => {
+    const getVariantClass = () => {
+        if (isIcon) "btn-icon";
+        
+        switch (variant) {
+            case 'secondary':
+                return 'btn-secondary';
+            case 'danger':
+                return 'btn-danger';
+            case 'outlined':
+                return 'btn-outlined';
+            case 'primary':
+            default:
+                return 'btn-primary'; 
+        }
+    };
+
+    const buttonClassName = [
+        'btn',
+        getVariantClass(),
+        className
+    ].join(' ');
+
+    return (
+        <button
+            className={buttonClassName}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
