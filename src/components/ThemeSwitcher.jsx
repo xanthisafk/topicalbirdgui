5baa0bed034a7ff99161556a6b33733203bbf429
Abhinav <abhinav.ranger@gmail.com>
2025-11-04 15:21:43 +0530

    Refactor: Theme switching transition

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -61,35 +61,11 @@ const ThemeSwitcher = () => {
       return;
     }
 
-    await document.startViewTransition(() => {
+    document.startViewTransition(() => {
       flushSync(() => {
         setCurrentTheme(value);
       });
-    }).ready;
-
-    const { top, left, width, height } = ref.current.getBoundingClientRect();
-    const x = left + width / 2;
-    const y = top + height / 2;
-    const right = window.innerWidth - left;
-    const bottom = window.innerHeight - top;
-    const maxRadius = Math.hypot(
-      Math.max(left, right),
-      Math.max(top, bottom),
-    );
-
-    document.documentElement.animate(
-      {
-        clipPath: [
-          `circle(0px at ${x}px ${y}px)`,
-          `circle(${maxRadius}px at ${x}px ${y}px)`,
-        ],
-      },
-      {
-        duration: 500,
-        easing: 'ease-in-out',
-        pseudoElement: '::view-transition-new(root)',
-      }
-    );
+    });
   }
 
   return (
