2771985f9e535cbcba9447023352a85bf8d6bf82
Abhinav <abhinav.ranger@gmail.com>
2025-11-06 03:00:03 +0530

    Update: Login Page

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,7 +1,39 @@
+import Button from '@/components/ui/Button';
+import InputBox from '@/components/ui/Input';
+import Label from '@/components/ui/Label';
+import { GUI_DEFAULT_IMAGES, NAVIGATION_PAGES } from '@/config';
+import useThemeIcon from '@/helpers/useThemeIcon';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import "@/styles/pages/login.css";
+import Checkbox from '@/components/ui/Checkbox';
 
 const Login = () => {
-  return <div>Login</div>;
+  const icon = useThemeIcon();
+  return (
+    <>
+      <div className='container'>
+        <div className='form'>
+          <img src={icon} alt={GUI_DEFAULT_IMAGES.appIcon.alt} width={50} />
+          <h3>Welcome back!</h3>
+          <div className='form-group'>
+            <Label for="email">Email</Label>
+            <InputBox type="email" id="email" placeholder="Enter your email..." />
+          </div>
+          <div className='form-group'>
+            <Label for="password">Password</Label>
+            <InputBox type="password" id="password" placeholder="Enter your password..." />
+          </div>
+          <Checkbox label="Remember Me" />
+          <Button variant='primary'>Login</Button>
+          <Link to={NAVIGATION_PAGES.auth.register} viewTransition>
+            <Button variant='secondary'>Don't have an account?</Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default Login;
--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -3,14 +3,17 @@ import useThemeIcon from "@/helpers/useThemeIcon";
 import "@/styles/components/ui/title.css";
 import { Link } from "react-router-dom";
 
-export const TitleText = () => {
+export const TitleText = ({ showText = true }) => {
     const icon = useThemeIcon();
     return (
         <Link to={NAVIGATION_PAGES.home} viewTransition className="title-container">
             <img src={icon} alt={GUI_DEFAULT_IMAGES.appIcon.alt} className="title-icon" />
-            <p className="typewriter-text">{
-                SITE_TITLE.toUpperCase().split("").map((c, i) => (<span key={i}>{c}</span>))
-            }</p>
+            {
+                showText &&
+                <p className="typewriter-text">{
+                    SITE_TITLE.toUpperCase().split("").map((c, i) => (<span key={i}>{c}</span>))
+                }</p>
+            }
         </Link>
     )
 }
