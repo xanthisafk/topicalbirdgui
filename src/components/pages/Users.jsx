1e0e4b058770e837ad5d4b41212153fd7626ad87
Abhinav <abhinav.ranger@gmail.com>
2025-11-06 08:11:31 +0530

    Add: User Profile

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Loader from '../ui/Loader';
+import { API_DEFAULT_IMAGES, API_URL_FROM_CONTENT_URL, NAVIGATION_PAGES } from '@/config';
+import { getUserbyUsername } from '@/helpers';
 
 const Users = () => {
   const { username } = useParams();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [userIcon, setUserIcon] = useState(API_DEFAULT_IMAGES.userPicture.image);
+
+  useEffect(() => {
+    if (!username) {
+      navigate(NAVIGATION_PAGES.home, { viewTransition: true });
+      return;
+    }
+
+    const fetchUser = async () => {
+      try {
+        const res = await getUserbyUsername(username);
+        if (res.status === 200) {
+          setUser(res.data.content);
+          setUserIcon(API_URL_FROM_CONTENT_URL(user.icon));
+          return;
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUser();
+  }, [navigate, username]);
+
   return (
-    <h1>User {username}'s Page</h1>
+    <>
+      {loading ? <div><Loader /><br />Loading {username}'s profile...</div>
+        : user === null ? <div>This user doesn't exist!</div>
+          : <div>
+            <h3>{user.displayName}</h3>
+            <h6>u/{user.handle}</h6>
+            <h6>Since: {new Date(user.createdAt).toLocaleDateString('en-US', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })}</h6>
+            <img src={userIcon} alt={`${user.handle}'s profile picture.`} width={250} height={250} />
+            {JSON.stringify(user)}
+          </div>}
+    </>
   );
 };
 
